ec0c654f62e650e60ee5c1c4aaa13e94431fcdb8
dachieng <oloodorcas99@gmail.com>
2022-03-14 18:38:11 +0300

    change models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="pics", default="pics/default.jpg")
+    image = models.ImageField(default="default.jpg")
     description = models.TextField(max_length=200)
 
     def __str__(self):
         return f"{self.user.username} Profile"
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.name)
-
-        if img.width > 300 or img.height > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.name)
+  
