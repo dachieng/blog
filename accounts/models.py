732d3880a51d1acb4a96d00d934fd372dd134413
dachieng <oloodorcas99@gmail.com>
2022-03-14 19:17:29 +0300

    add media files

--- accounts/models.py
+++ accounts/models.py
@@ -10,12 +10,4 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.name)
-
-        if img.width > 300 or img.height > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.name)
+   
