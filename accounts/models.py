16d65afbff8662dfd6046bc5e72efe80cb96c6bc
dachieng <oloodorcas99@gmail.com>
2022-03-14 19:02:04 +0300

    resize image

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,6 +8,14 @@ class Profile(models.Model):
     description = models.TextField(max_length=200)
 
     def __str__(self):
-        return f"{self.user.username} Profile"
+        return f"{self.user.username}'s Profile"
 
-  
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.name)
+
+        if img.width > 300 or img.height > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.name)
